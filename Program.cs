8a6b8d03ae67e8b56fbbb19e2780a60120e4b46f
mizterwaffles <nhillhouse04@gmail.com>
2025-11-05 17:38:36 -0700

    touched up movement and collision

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿/*
 Nathan Hillhouse
 10/24/2025
-Lab 8 - Maze
+Lab 9 - Maze 2
 */
 
 
@@ -10,10 +10,12 @@ Console.WriteLine("Welcome to the Maze! You will use your arrow keys to navigate
 Console.WriteLine();
 
 string[] maprows = File.ReadAllLines("./maze.txt");
-foreach (string item in maprows) Console.WriteLine(item);
-(int x, int y) buffer = (1, 2);
+(int x, int y) buffer = (2, 2);
 (int x, int y) cursorPosition = (buffer.x, buffer.y);
+foreach (string item in maprows) Console.WriteLine(item);
 
+int rows = maprows.Length;
+int columns = maprows[0].Length;
 Console.SetCursorPosition(cursorPosition.x, cursorPosition.y);
 
 do
@@ -31,23 +33,40 @@ do
 }
 while (true);
 
-Console.SetCursorPosition(0, 10);
+Console.SetCursorPosition(0, rows + 1);
 Console.WriteLine("You win!");
 
 
 static (int, int) CheckKeys((int x, int y) cursorPosition, ConsoleKey key, (int x, int y) buffer, string[] maprows)
 {
-    Console.Write(maprows[cursorPosition.y-buffer.y][cursorPosition.x]);
-    if (key == ConsoleKey.UpArrow && (maprows[cursorPosition.y-buffer.y-1][cursorPosition.x] != '*' && maprows[cursorPosition.y-buffer.y-1][cursorPosition.x] !='|')) cursorPosition.y--;
-    else if (key == ConsoleKey.DownArrow && (maprows[cursorPosition.y-buffer.y-1][cursorPosition.x] != '*' && maprows[cursorPosition.y-buffer.y-1][cursorPosition.x] !='|')) cursorPosition.y++;
-    else if (key == ConsoleKey.RightArrow&& (maprows[cursorPosition.y-buffer.y-1][cursorPosition.x] != '*' && maprows[cursorPosition.y-buffer.y-1][cursorPosition.x] !='|')) cursorPosition.x++;
-    else if (key == ConsoleKey.LeftArrow&& (maprows[cursorPosition.y-buffer.y-1][cursorPosition.x] != '*' && maprows[cursorPosition.y-buffer.y-1][cursorPosition.x] !='|')) cursorPosition.x--;
+    (int x, int y) map = (cursorPosition.y - buffer.y, cursorPosition.x - buffer.x);
+    char locationUp = ' ';
+    char locationDown = ' ';
+    char locationRight = ' ';
+    char locationLeft = ' ';
 
-    
+    if (map.y > 0) 
+        locationUp = maprows[cursorPosition.y - buffer.y-1][cursorPosition.x];
+    if (map.y < maprows.Length - 1) 
+        locationDown = maprows[cursorPosition.y - buffer.y+1][cursorPosition.x];
+    if (map.x < maprows.Length - 1)
+        locationRight = maprows[cursorPosition.y - buffer.y][cursorPosition.x + 1];
+    if (map.x > 0)
+        locationLeft = maprows[cursorPosition.y - buffer.y][cursorPosition.x - 1];
+
+    if (key == ConsoleKey.UpArrow && locationUp != '*' && locationUp != '|') cursorPosition.y--;
+    else if (key == ConsoleKey.DownArrow && locationDown != '*' && locationDown != '|') cursorPosition.y++;
+    else if (key == ConsoleKey.RightArrow && locationRight != '*' && locationRight != '|') cursorPosition.x++;
+    else if (key == ConsoleKey.LeftArrow && locationLeft != '*' && locationLeft != '|') cursorPosition.x--;
+
+
     if (cursorPosition.x < buffer.x) cursorPosition.x = buffer.x;
-    else if (cursorPosition.x > maprows[0].Length) cursorPosition.x = maprows[0].Length;
+    else if (cursorPosition.x >= maprows[0].Length) cursorPosition.x = maprows[0].Length;
     if (cursorPosition.y < buffer.y) cursorPosition.y = buffer.y;
-    else if (cursorPosition.y > maprows.Length) cursorPosition.y = maprows.Length;// + buffer.y;
+    else if (cursorPosition.y >= maprows.Length) cursorPosition.y = maprows.Length;// + buffer.y;
+
+    Console.SetCursorPosition(0, 0);
+    Console.WriteLine($"U:{locationUp}, D:{locationDown}, L:{locationLeft}, R:{locationRight}          ");
     Console.SetCursorPosition(cursorPosition.x, cursorPosition.y);
 
 
